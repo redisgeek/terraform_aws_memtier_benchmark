6c84630dd105c3d3ba632a01daaa7e34afce14f3
DaShaun <dashaun@dashaun.com>
2021-11-12 11:39:18 -0600

    remove version requirement and add defaults

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
-variable "region" {
-    description = "Region into which to deploy the EC2 instance"
+variable "aws_region" {
+  description = "Region into which to deploy the EC2 instance"
+  default     = "us-east-1"
 }
 
 variable "linux_ami" {
-    description = "Linux ami to use"
+  description = "Linux ami to use :: Should work with Redis Labs Sales Account"
+  default     = "ami-038f1ca1bd58a5790"
 }
 
 variable "instance_type" {
